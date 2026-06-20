4c9379ceffafad58271f38b3a7b969b0cccfd2ed
mvakili96 <mohammadjavad.gh.vakili@gmail.com>
2026-06-20 11:26:33 -0400

    Move ground-truth helper into shared evaluation package

--- ptsemseg/evaluation/__init__.py
+++ ptsemseg/evaluation/__init__.py
@@ -3,6 +3,7 @@
 from ptsemseg.evaluation.geometry import MyIPM
 from ptsemseg.evaluation.geometry import MyUtil
 from ptsemseg.evaluation.geometry import MyUtils_3D
+from ptsemseg.evaluation.ground_truth import MyHelper_GT
 from ptsemseg.evaluation.types import TYPE_path
 from ptsemseg.evaluation.visualization import adjust_rgb_for_region
 from ptsemseg.evaluation.visualization import rectify_pixel_value
@@ -12,6 +13,7 @@ from ptsemseg.evaluation.vsa import create_VSAObject_from_PE_results
 
 __all__ = [
     "MyIPM",
+    "MyHelper_GT",
     "MyUtil",
     "MyUtils_3D",
     "Polygon_dummy",
