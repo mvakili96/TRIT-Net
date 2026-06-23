05fd97b9ed22b1097d2c6cd7fd244d7197749557
mvakili96 <mohammadjavad.gh.vakili@gmail.com>
2026-06-23 15:32:58 -0400

    Move demo eval metrics into shared evaluation package

--- evaluation/code_TPEnet_PathExtraction/helpers/utils/my_utils_RPG.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility wrapper for legacy rail-path-graph helper."""
-
-from ptsemseg.evaluation.rail_path_graph import MyUtils_RailPathGraph
-from ptsemseg.evaluation.types import TYPE_path
-
-__all__ = ["MyUtils_RailPathGraph", "TYPE_path"]
--- evaluation/code_TPEnet_PathExtraction/helpers/utils/rail_path_graph/my_RPG_core.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility wrapper for legacy rail-path-graph core helpers."""
-
-from ptsemseg.evaluation.rail_path_graph_core import DLList_RPG
-from ptsemseg.evaluation.rail_path_graph_core import TYPE_node
-
-__all__ = ["DLList_RPG", "TYPE_node"]
